3358eff3baa0dba812b8fc5d7596fa64f33d568e
Andy Xie <andy.xie.opensource@gmail.com>
2023-08-06 17:02:18 +0800

    Add generic test parameters

--- {{ cookiecutter.project_slug }}/tests/test_api.py
+++ {{ cookiecutter.project_slug }}/tests/test_api.py
@@ -4,6 +4,14 @@ import pytest
 
 from {{ cookiecutter.project_slug }} import api
 
+
+@pytest.mark.parametrize(
+    [...],
+    (
+        [...],
+        [...],
+    )
+)
 def test_{{ cookiecutter.problem_api }}(...) -> None
     """Tests solution for problem {{ cookiecutter.problem_name }}"""
 
